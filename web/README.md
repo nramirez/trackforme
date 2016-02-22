457344d2554f05ca16e6cafb02eefaf608abbc49
vetom <tomas.veras@me.com>
2016-02-21 20:42:25 -0400

    Web app is now using gulp and ready for the server side.

---
+++ web/README.md
@@ -0,0 +1,1 @@
+###TrackForMe Web 
---
+++ web/src/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import exphbs from 'express-handlebars';
+
+const app = express();
+const PORT = 8000;
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use('/', express.static(__dirname + '/public'));
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(PORT, () => {
+  console.log('Server is listening on port: ', PORT);
+});
+
+//routes
+app.get('/', function (req, res) {
+    res.render('home');
+});
