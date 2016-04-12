8c9ce611a365984f01306fea255e4dafb31e8921
manuel mejia <mejiamanuel57@outlook.com>
2016-04-12 16:42:57 -0400

    45: Catch sites without email and avoid duplication

--- chrome/lib/scripts/options.js
+++ chrome/lib/scripts/options.js
@@ -1,48 +1,50 @@
 import BackStore from './background-store';
 
 const setupUserSettings = (userSettings) => {
+  if (userSettings.email) {
     document.getElementById('email-input').value = userSettings.email;
     document.getElementById('time-input').value = userSettings.trackingTime;
-    displaySites(userSettings.sites);
+  }
+  displaySites(userSettings.sites);
 };
 
 const validEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 document.querySelector('#save-user-settings')
-    .addEventListener('click', function(event) {
-        let email = document.getElementById('email-input').value;
-        let trackingTime = document.getElementById('time-input').value;
-        let userSettings = {
-            email: email,
-            trackingTime: trackingTime
-        };
-        if (validEmail.test(email)) {
-            BackStore.SaveUserSettings(userSettings);
-        } else {
-            document.getElementById('email-error').innerHTML = 'Invalid email';
-        }
-    });
+  .addEventListener('click', function(event) {
+    let email = document.getElementById('email-input').value;
+    let trackingTime = document.getElementById('time-input').value;
+    let userSettings = {
+      email: email,
+      trackingTime: trackingTime
+    };
+    if (validEmail.test(email)) {
+      BackStore.SaveUserSettings(userSettings);
+    } else {
+      document.getElementById('email-error').innerHTML = 'Invalid email';
+    }
+  });
 
 document.querySelector('#email-input')
-    .addEventListener('focus', function(event) {
-        document.getElementById('email-error').innerHTML = '';
-    });
+  .addEventListener('focus', function(event) {
+    document.getElementById('email-error').innerHTML = '';
+  });
 
 function displaySites(sites) {
-    let innerTable = '';
-    if (!sites || Object.keys(sites).length == 0) {
-        innerTable = '<tr><td colspan="2">Please add your sites.</td></tr>';
-    } else {
-        for (let k in sites) {
-            let site = sites[k];
-            innerTable += sideRow(site.img, site.url);
-        }
+  let innerTable = '';
+  if (!sites || Object.keys(sites).length == 0) {
+    innerTable = '<tr><td colspan="2">Please add your sites.</td></tr>';
+  } else {
+    for (let k in sites) {
+      let site = sites[k];
+      innerTable += sideRow(site.img, site.url);
     }
-    document.getElementById('sites-tbody').innerHTML = innerTable;
+  }
+  document.getElementById('sites-tbody').innerHTML = innerTable;
 }
 
 const sideRow = (previewUrl, siteUrl) =>
-    `<tr>
+  `<tr>
     <td>
       <img class="img-preview" src="${previewUrl}" />
     </td>
@@ -55,5 +57,5 @@ const sideRow = (previewUrl, siteUrl) =>
   </tr>`;
 
 BackStore.Load(function(response) {
-    setupUserSettings(response.config);
+  setupUserSettings(response.config);
 });
