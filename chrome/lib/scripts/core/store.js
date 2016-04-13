47293efa431be1d1f75cd7c3a4174cd68271349d
manuel mejia <mejiamanuel57@outlook.com>
2016-04-13 16:15:39 -0400

    45: Catching trackings without email

--- chrome/lib/scripts/core/store.js
+++ chrome/lib/scripts/core/store.js
@@ -6,77 +6,92 @@ const USERCONFIG = 'USERCONFIG';
 const CURRENTRACKING = 'CURRENTRACKING';
 
 const Store = {
-    LoadUserSettings(callback) {
-        //By default try to local storage, in case the ajax request fails
-        let config = amplify(USERCONFIG) || {};
-        if (config.email) {
-            //Try to load the user info from the server
-            $.get(`${ServerBaseUrl}/users/${config.email}`, (response) => {
-                config = response;
-                //Update localstorage
-                amplify(USERCONFIG, config);
-            }).always(function() {
-                //Return config, either was loaded from the server or the local storage
-                callback(config);
-            });
-        }
-    },
-
-    //Save tracking in the local storage
-    SaveCurrentTracking(trackingElements) {
-        console.log('saving tracking in progress', trackingElements);
-        return amplify(CURRENTRACKING, trackingElements);
-    },
-
-    //Load tracking in the local storage
-    LoadCurrentTracking() {
-        return amplify(CURRENTRACKING);
-    },
-
-    //Persist trackings on the server
-    PostTrackings(trackings, callback) {
-        let config = amplify(USERCONFIG);
-        console.log(trackings);
-        if (!config || !config.email || !trackings) {
-            callback(false);
-        } else {
-            let trackingPayload = {
-                email: config.email,
-                trackings: trackings
-            };
-            $.post(`${ServerBaseUrl}/trackings`, {
-                trackingPayload: trackingPayload
-            }, (response) => {
-                console.log(response);
-                callback(true);
-            });
-        }
-    },
-
-    SaveImage(image, callback) {
-        $.post(`${ServerBaseUrl}/trackings/image`, {
-            image: image
-        }, (url) => {
-            callback(null, url);
-        });
-
-    },
-
-    _saveUserConfig(config) {
-        return amplify(USERCONFIG, config)
-    },
-
-    SaveUserSettings(userSettings, callback) {
-        $.post(`${ServerBaseUrl}/users`, {
-            email: userSettings.email,
-            trackingTime: userSettings.trackingTime
-        }).always(() => {
-            this._saveUserConfig({
-                email: userSettings.email,
-                trackingTime: userSettings.trackingTime
-            });
-        });
+  LoadUserSettings(callback) {
+    //By default try to local storage, in case the ajax request fails
+    let config = amplify(USERCONFIG) || {};
+    if (config.email) {
+      //Try to load the user info from the server
+      $.get(`${ServerBaseUrl}/users/${config.email}`, (response) => {
+        config = response;
+        //Update localstorage
+        amplify(USERCONFIG, config);
+      }).always(function() {
+        //Return config, either was loaded from the server or the local storage
+        callback(config);
+      });
+    } else {
+      callback(config);
     }
+  },
+
+  //Save tracking in the local storage
+  SaveCurrentTracking(trackingElements) {
+    console.log('saving tracking in progress', trackingElements);
+    return amplify(CURRENTRACKING, trackingElements);
+  },
+
+  //Load tracking in the local storage
+  LoadCurrentTracking() {
+    return amplify(CURRENTRACKING);
+  },
+
+  //Persist trackings on the server
+  PostTrackings(trackings, callback) {
+    let config = amplify(USERCONFIG) || {};
+    // but first, save it local in the USERCONFIG
+    config.trackings = trackings;
+    amplify(USERCONFIG, config);
+
+    console.log(trackings);
+    if (!config || !config.email || !trackings) {
+      callback(false);
+    } else {
+      let trackingPayload = {
+        email: config.email,
+        trackings: trackings
+      };
+      $.post(`${ServerBaseUrl}/trackings`, {
+        trackingPayload: trackingPayload
+      }, (response) => {
+        console.log(response);
+        //clean the currentTrackings to avoid duplications
+        this.SaveCurrentTracking(null);
+        callback(true);
+      });
+    }
+  },
+
+  SaveImage(image, callback) {
+    $.post(`${ServerBaseUrl}/trackings/image`, {
+      image: image
+    }, (url) => {
+      callback(null, url);
+    });
+
+  },
+
+  _saveUserConfig(config) {
+    return amplify(USERCONFIG, config)
+  },
+
+  SaveUserSettings(userSettings, callback) {
+    if (!userSettings.email) {
+      callback(false);
+    } else {
+      $.post(`${ServerBaseUrl}/users`, {
+        email: userSettings.email,
+        trackingTime: userSettings.trackingTime
+      }).always(() => {
+        this._saveUserConfig({
+          email: userSettings.email,
+          trackingTime: userSettings.trackingTime
+        });
+        //save the currentTrackings in the server
+        this.PostTrackings(this.LoadCurrentTracking());
+        callback(true);
+      });
+    }
+  }
 };
 
 export default Store;
--- chrome/lib/scripts/options.js
+++ chrome/lib/scripts/options.js
@@ -1,53 +1,55 @@
 import BackStore from './core/background-store';
 
 const setupUserSettings = (userSettings) => {
+  if (userSettings.email) {
     document.getElementById('email-input').value = userSettings.email;
     document.getElementById('time-input').value = userSettings.trackingTime;
-    displayTrackings(userSettings.trackings);
+  }
+  displayTrackings(userSettings.trackings);
 };
 
 const validEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 document.getElementById('save-user-settings')
-    .addEventListener('click', (event) => {
-        let email = document.getElementById('email-input').value;
-        let trackingTime = document.getElementById('time-input').value;
-        let userSettings = {
-            email: email,
-            trackingTime: trackingTime
-        };
-        if (validEmail.test(email)) {
-            BackStore.SaveUserSettings(userSettings);
-        } else {
-            document.getElementById('email-error').innerHTML = 'Invalid email';
-        }
-    });
+  .addEventListener('click', (event) => {
+    let email = document.getElementById('email-input').value;
+    let trackingTime = document.getElementById('time-input').value;
+    let userSettings = {
+      email: email,
+      trackingTime: trackingTime
+    };
+    if (validEmail.test(email)) {
+      BackStore.SaveUserSettings(userSettings);
+    } else {
+      document.getElementById('email-error').innerHTML = 'Invalid email';
+    }
+  });
 
 document.getElementById('email-input')
-    .addEventListener('focus', (event) => {
-        document.getElementById('email-error').innerHTML = '';
-    });
+  .addEventListener('focus', (event) => {
+    document.getElementById('email-error').innerHTML = '';
+  });
 
 document.getElementById('tracking-tigger')
-    .addEventListener('click', (e) => {
-      BackStore.RunTracking();
-    });
+  .addEventListener('click', (e) => {
+    BackStore.RunTracking();
+  });
 
 function displayTrackings(trackings) {
-    let innerTable = '';
-    if (!trackings || Object.keys(trackings).length == 0) {
-        innerTable = '<tr><td colspan="2">Please add what you need to track. Watch how to do it in this video.</td></tr>';
-    } else {
-        for (let k in trackings) {
-            let tracking = trackings[k];
-            innerTable += trackingRow(tracking.img, tracking.url);
-        }
+  let innerTable = '';
+  if (!trackings || Object.keys(trackings).length == 0) {
+    innerTable = '<tr><td colspan="2">Please add what you need to track. Watch how to do it in this video.</td></tr>';
+  } else {
+    for (let k in trackings) {
+      let tracking = trackings[k];
+      innerTable += trackingRow(tracking.img, tracking.url);
     }
-    document.getElementById('trackings-tbody').innerHTML = innerTable;
+  }
+  document.getElementById('trackings-tbody').innerHTML = innerTable;
 }
 
 const trackingRow = (imgUrl, url) =>
-    `<tr>
+  `<tr>
     <td>
       <img class="img-preview" src="${imgUrl}" />
     </td>
@@ -60,5 +62,5 @@ const trackingRow = (imgUrl, url) =>
   </tr>`;
 
 BackStore.LoadUserSettings((response) => {
-    setupUserSettings(response.config);
+  setupUserSettings(response.config);
 });
