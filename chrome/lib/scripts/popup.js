2f2796763928c02da9312d13f152fe1397c79b8c
wistcc <wistcc@hotmail.com>
2016-04-21 11:47:49 -0400

    71: Image is not always saved if  button is clicked supper fast.
    Improving implementation due to suggestions on the pull request
    
    Addressing last comments

--- chrome/lib/scripts/popup.js
+++ chrome/lib/scripts/popup.js
@@ -1,31 +1,35 @@
 import BackStore from './core/background-store';
 
 let config = {};
-let currentTracking = {};
 BackStore.LoadUserSettings((response) => {
-  config = response.config;
+    config = response.config;
 });
 
-BackStore.LoadCurrentTracking((response) => {
-  currentTracking = response.currentTracking;
-  console.log(currentTracking);
-});
-
-document.querySelector('.btn-done')
-  .addEventListener('click', (event) => {
-    console.log('the done', config);
-    if (currentTracking) {
-      BackStore.PostTrackings(currentTracking, () => {
-        chrome.tabs.create({
-          url: chrome.extension.getURL('/views/options.html')
-        });
-      });
-    }
-  });
+document.querySelector('.btn-done').addEventListener('click', event => submitTrackings());
 
 document.getElementById('btn-options')
-  .addEventListener('click', () => {
-    chrome.tabs.create({
-      url: chrome.extension.getURL('/views/options.html')
+    .addEventListener('click', () => {
+        chrome.tabs.create({
+            url: chrome.extension.getURL('/views/options.html')
+        });
     });
-  });
+
+const submitTrackings = () => {
+    BackStore.LoadCurrentTracking((response) => {
+        var areAllImagesLoaded = Object.keys(response.currentTracking).find(key => !response.currentTracking[key].img);
+
+        if (!areAllImagesLoaded) {
+            if (response.currentTracking) {
+                BackStore.PostTrackings(response.currentTracking, () => {
+                    chrome.tabs.create({
+                        url: chrome.extension.getURL('/views/options.html')
+                    });
+                });
+            }
+        } else {
+            document.querySelector('.btn-done').classList.add('btn-disabled');
+            document.getElementById('savingTrackingMessage').classList.remove('none');
+            setTimeout(submitTrackings, 1000);
+        }
+    });
+}
